9a52744ec04b77ab202305060678f18929fabf99
Simon <simon@zenoss.com>
2012-04-09 14:58:48 -0500

    fixing stuff

--- chef-repo/cookbooks/mysql/recipes/cleanup_mysql.rb
+++ chef-repo/cookbooks/mysql/recipes/cleanup_mysql.rb
@@ -8,7 +8,7 @@
 # cleanup the old mysql stuff on the host first
 # this is currently not platform specific 
 # TODO: make this work on all platforms
-old_mysql_packages = %w{ mysql-client mysql-client-5.1 mysql-client-core-5.1 mysql-common mysql-server mysql-server-5.1 mysql-server-core-5.1 mysql-connector-odbc mysql-libs }
+old_mysql_packages = %w{ mysql-client-5.1 mysql-client-core-5.1 mysql-common mysql-server mysql-server-5.1 mysql-server-core-5.1 mysql-connector-odbc mysql-libs }
 
 old_mysql_packages.each do |pkg|
   package pkg do
